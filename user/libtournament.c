d2a0cba09c2cf217d28a58c4a750b2eda6e0d454
moshe2432 <moshekl@post.bgu.ac.il>
2025-05-22 05:16:27 +0000

    delete tree

--- user/libtournament.c
+++ user/libtournament.c
@@ -12,17 +12,16 @@ struct tournament_tree
     int id;
     int num_processes;
     struct tournament_tree *next;
-    int* treeNodes;
-    int* processes;
+    int *treeNodes;
+    int *processes;
 };
 
 struct tournament_tree *trees;
 
+#define MAX_LEVELS 4 // log2(16)
 
-
-#define MAX_LEVELS 4  // log2(16)
-
-struct lock_path_entry {
+struct lock_path_entry
+{
     int lock_id;
     int role; // 0 or 1
 };
@@ -31,13 +30,11 @@ struct lock_path_entry {
 struct lock_path_entry path[MAX_LEVELS];
 int path_length = 0;
 
-
-
-
-
-int log2_int(unsigned int x) {
+int log2_int(unsigned int x)
+{
     int res = 0;
-    while (x >>= 1) {
+    while (x >>= 1)
+    {
         res++;
     }
     return res;
@@ -49,7 +46,6 @@ int get_role(int index, int L, int l)
     role = (index & (1 << (L - l - 1))) >> (L - l - 1);
     return role;
 }
-
 
 // Creates a new tournament tree with the given number of processes.
 // This function accepts the number of processes that will participate in
@@ -73,34 +69,42 @@ int tournament_create(int processes)
     tree->id = id_counter++;
     tree->num_processes = processes;
 
-    for (int i = 0; i < processes - 1; i++) {
+    for (int i = 0; i < processes - 1; i++)
+    {
         tree->treeNodes[i] = peterson_create();
     }
 
-    for (int i = 1; i < processes; i++) {
+    for (int i = 1; i < processes; i++)
+    {
         pid = fork();
-        if (pid < 0) return -1;
+        if (pid < 0)
+            return -1;
 
-        if (pid == 0) {
+        if (pid == 0)
+        {
             // In child
-            for (int l = 0; l < L; l++) {
-                int parent = (i >> (l+1)) + (1 << (L - l - 1)) - 1;
+            for (int l = 0; l < L; l++)
+            {
+                int parent = (i >> (l + 1)) + (1 << (L - l - 1)) - 1;
                 int role = (i >> l) & 1;
                 path[l].lock_id = tree->treeNodes[parent];
                 path[l].role = role;
             }
             path_length = L;
             break;
-        } else {
+        }
+        else
+        {
             tree->processes[i] = pid;
         }
     }
 
     tree->processes[0] = getpid();
-    if(pid !=0)
+    if (pid != 0)
     {
         // In parent
-        for (int l = 0; l < L; l++) {
+        for (int l = 0; l < L; l++)
+        {
             int role = get_role(0, L, l);
             path[l].lock_id = tree->treeNodes[(1 << (L - l - 1)) - 1];
             path[l].role = role;
@@ -119,24 +123,40 @@ int tournament_create(int processes)
 int tournament_acquire(void)
 {
 
-    for (int i = 0; i < path_length; i++) {
+    for (int i = 0; i < path_length; i++)
+    {
         peterson_acquire(path[i].lock_id, path[i].role);
     }
     return 0;
-    
 }
-
 
 // Releases all locks held by the calling process in the reverse order of
 // acquisition. Returns 0 on success and -1 on error.
 int tournament_release(void)
 {
-    for (int i = path_length - 1; i >= 0; i--) {
+    for (int i = path_length - 1; i >= 0; i--)
+    {
         peterson_release(path[i].lock_id, path[i].role);
     }
     return 0;
-
- 
 }
 
+int tournament_delete(int id)
+{
+    struct tournament_tree *tree = trees;
 
+    while (tree->id != id)
+    {
+        if (tree->next == 0)
+        {
+            return -1;
+        }
+        tree = tree->next;
+    }
+
+    for (int i = 0; i < tree->num_processes - 1; i++)
+    {
+        peterson_destroy(tree->treeNodes[i]);
+    }
+    return 0;
+}
--- user/tournament.c
+++ user/tournament.c
@@ -1,20 +1,20 @@
- #include "kernel/types.h"
+#include "kernel/types.h"
 #include "kernel/stat.h"
- #include "user.h"
+#include "user.h"
 
-
- int main(int argc, char *argv[])
- {
-    int prosNum;
+int main(int argc, char *argv[])
+{
+    int prosNum, pid = getpid();
     printf("argc: %d\n", argc);
 
-    if(argc > 1){
+    if (argc > 1)
+    {
         prosNum = atoi(argv[1]);
     }
-    else{
+    else
+    {
         prosNum = 16;
     }
-
 
     int id = tournament_create(prosNum);
     if (id < 0)
@@ -22,30 +22,32 @@
         printf("Failed to create tournament\n");
         exit(-1);
     }
- 
-    //printf("Tournament created with ID: %d\n", id);
- 
+
+    // printf("Tournament created with ID: %d\n", id);
+
     // Acquire the lock
     if (tournament_acquire() < 0)
-     {
-         printf("Failed to acquire lock\n");
-         exit(-1);
-     }
- 
-     printf("Process %d: Lock acquired\n", getpid());
- 
-     // Simulate some work
-     sleep(1);
-     printf("Process %d: Lock release\n", getpid());
-     // Release the lock
-     if (tournament_release() < 0)
-     {
-         printf("Failed to release lock\n");
-         exit(-1);
-     }
- 
-     
- 
-     exit(0);
- }
+    {
+        printf("Failed to acquire lock\n");
+        exit(-1);
+    }
 
+    printf("Process %d: Lock acquired\n", getpid());
+
+    // Simulate some work
+    sleep(1);
+    printf("Process %d: Lock release\n", getpid());
+    // Release the lock
+    if (tournament_release() < 0)
+    {
+        printf("Failed to release lock\n");
+        exit(-1);
+    }
+
+    if (getpid() == pid)
+    {
+        tournament_delete(id);
+    }
+
+    exit(0);
+}
